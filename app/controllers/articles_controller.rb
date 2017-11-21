fcba29547c454499d3337eca143d505beccd2e4a
Francis Berger <frcs.berger@gmail.com>
2017-11-21 23:35:30 +0100

    revue des exos

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,20 +4,43 @@ class ArticlesController < ApplicationController
   end
 
   def show          # GET /articles/:id
+    get_article
   end
 
   def new           # GET /articles/new
+    @article = Article.new
   end
 
   def create        # POST /articles
+    @article = Article.new(article_params)
+    @article.save
+    redirect_to articles_path
   end
 
   def edit          # GET /articles/:id/edit
+    get_article
   end
 
   def update        # PATCH /articles/:id
+    @article = get_article
+    @article.update(article_params)
+    redirect_to articles_path
   end
 
   def destroy       # DELETE /articles/:id
+    @article = get_article
+    @article.destroy
+    redirect_to articles_path
   end
+
+  private
+
+  def get_article
+    @article = Article.find(params[:id])
+  end
+
+  def article_params
+    params.require(:article).permit(:title, :contnet)
+  end
+
 end
